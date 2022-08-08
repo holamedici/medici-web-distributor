7f69f19d038c594a0b2cca5f3a555bb1f299c0b4
Simon Mahns <ak.simonm@gmail.com>
2022-08-08 06:48:29 -0400

    placeholder

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div
+     className="main"
+    >
+      <div>
+        <h1 className="generalTitle">MEDICI DISTRIBUTOR PLATFORM</h1>
+        <p className="smallTitle">COMING SOON</p>
+      </div>
+    </div>
+  );
+}
+export default App;
